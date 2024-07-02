31c481fb67a9ee9b34a6b90e27001732ea41a4eb
Tayná Vicente Silva <94538752+tayna01@users.noreply.github.com>
2024-07-02 20:59:55 -0300

    Add files via upload

---
+++ arquivoUm.py
@@ -0,0 +1,11 @@
+nome = "Tayná"
+data_nascimento = "05/02/2002"
+idade = 20
+salario = 5000
+arLigado = False
+
+print("Olá" , nome)
+print("Nascido em", data_nascimento)
+print("Está com", idade, "anos.")
+print("Recebe por mês R$" , salario)
+print("Deixou o ar ligado?" , arLigado)
